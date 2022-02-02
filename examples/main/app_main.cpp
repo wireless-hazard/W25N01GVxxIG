0b503ba603b291c92296f310073eab1774ba8b14
Magno <magnocostamaia@gmail.com>
2022-02-02 12:19:14 -0300

    EXAMPLE: Base deep sleep loop for the write/read operations created

--- examples/main/app_main.cpp
+++ examples/main/app_main.cpp
@@ -3,9 +3,37 @@
 #include "freertos/task.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "esp_sleep.h"
 
 #include "W25N01GV.h"
 
+constexpr static uint8_t sleep_seconds = 2;
+
+static const char *TAG = "w25_memory";
+
+RTC_DATA_ATTR static bool write;  
+
 extern "C" void app_main(){
+
+	esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+
+	if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER){
+		
+		if (write){
+
+			ESP_LOGI(TAG, "WRITE!");
+			write = false;
+
+		}else{
+
+			ESP_LOGI(TAG, "READ!");
+			write = true;
+		}
+
+	}else{
+		write = true; //Initializes the variable the very first time the esp is powered on
+	}
+
+	esp_deep_sleep(sleep_seconds*1000000); //Sleeps for 2 seconds
 
 }
